4171b41e4f33d16bbde5d771a24e260f649d0bb4
Estebancott <estebancott@gmail.com>
2023-12-18 23:24:26 +0100

    FIREBASE !!!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
 
 // import { AngularFireModule } from '@angular/fire/compat';
 // import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
@@ -29,14 +40,18 @@ export const firebaseConfig = {
   imports: [BrowserModule, 
           HttpClientModule,
             IonicModule.forRoot(), 
-            AppRoutingModule, 
+            AppRoutingModule, provideFirebaseApp(() => initializeApp({"projectId":"cuisinkoala","appId":"1:1079405041039:web:7b6ccec3a273e831cd27fa","storageBucket":"cuisinkoala.appspot.com","apiKey":"AIzaSyCsuu-bpyw6HMp3wI6rnGiXvE1GCDrDFno","authDomain":"cuisinkoala.firebaseapp.com","messagingSenderId":"1079405041039","measurementId":"G-908P10XBZ4"})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), provideAppCheck(() => {
+  // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_
+  const provider = new ReCaptchaEnterpriseProvider('6LfNtDUpAAAAAKWqx78scbM612AP3UAODHwW-k8t');
+  return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
+}), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), providePerformance(() => getPerformance()), provideStorage(() => getStorage()), provideRemoteConfig(() => getRemoteConfig()), 
             // AngularFireModule.initializeApp(firebaseConfig),
             // AngularFireDatabaseModule,
             // AngularFireAuthModule,
             // AngularFireStorageModule,
           ],
 
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ScreenTrackingService, UserTrackingService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
